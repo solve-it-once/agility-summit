3daae167cd05c167af55d1abdb326b5ba97dc282
Brad Czerniak <ao5357@gmail.com>
2024-07-03 11:36:43 -0400

    Resolves #0 by checking if tippy exists.

--- ct-readme.js
+++ ct-readme.js
@@ -61,35 +61,37 @@ window.addEventListener("load", (event) => {
    * Swap Solutions nav link for a dropdown button and put tippy on it.
    */
   function ready() {
-    var solutions = document.querySelectorAll(".rm-Header-bottom a.Button[href='/page/solutions']");
-    Array.prototype.forEach.call(solutions, (elem) => {
-      const dropdownRaw = document.createElement("div");
-      dropdownRaw.classList.add('Dropdown', 'Dropdown_closed', 'ct-solutions-dropdown');
-      dropdownRaw.innerHTML= `
-        <div class="Dropdown-toggle" aria-haspopup="dialog" aria-expanded="false">
-          <button class="rm-Header-link rm-Header-bottom-link undefined Button Button_slate_text Button_md" type="button">
-            <i class="icon-document2"></i>
-            <span>Solutions</span>
-            <i class="undefined icon-chevron-down"></i>
-          </button>
-        </div>
-      `;
-      elem.replaceWith(dropdownRaw);
-    });
+    if ('tippy' in window) {
+      var solutions = document.querySelectorAll(".rm-Header-bottom a.Button[href='/page/solutions']");
+      Array.prototype.forEach.call(solutions, (elem) => {
+        const dropdownRaw = document.createElement("div");
+        dropdownRaw.classList.add('Dropdown', 'Dropdown_closed', 'ct-solutions-dropdown');
+        dropdownRaw.innerHTML= `
+          <div class="Dropdown-toggle" aria-haspopup="dialog" aria-expanded="false">
+            <button class="rm-Header-link rm-Header-bottom-link undefined Button Button_slate_text Button_md" type="button">
+              <i class="icon-document2"></i>
+              <span>Solutions</span>
+              <i class="undefined icon-chevron-down"></i>
+            </button>
+          </div>
+        `;
+        elem.replaceWith(dropdownRaw);
+      });
 
-    tippy('.ct-solutions-dropdown', {
-      allowHTML: true,
-      arrow: false,
-      content: `<ul>
-        <li><a href="/docs/introduction">Inventory Management Labor &amp; Scheduling</a></li>
-        <li><a href="https://developer.zenput.com/docs/introduction">Operations Execution (Zenput)</a></li>
-        <li><a href="https://talentqa1-api.discoverlink.com/swagger/ui/index">Learning &amp; Development</a></li>
-        <li><a href="https://talentqa1-api.discoverlink.com/swagger/ui/index">Cruise</a></li>
-      </ul>`,
-      interactive: true,
-      placement: 'bottom',
-      theme: 'light',
-      trigger: 'click'
-    });
+      tippy('.ct-solutions-dropdown', {
+        allowHTML: true,
+        arrow: false,
+        content: `<ul>
+          <li><a href="/docs/introduction">Inventory Management Labor &amp; Scheduling</a></li>
+          <li><a href="https://developer.zenput.com/docs/introduction">Operations Execution (Zenput)</a></li>
+          <li><a href="https://talentqa1-api.discoverlink.com/swagger/ui/index">Learning &amp; Development</a></li>
+          <li><a href="https://talentqa1-api.discoverlink.com/swagger/ui/index">Cruise</a></li>
+        </ul>`,
+        interactive: true,
+        placement: 'bottom',
+        theme: 'light',
+        trigger: 'click'
+      });
+    }
   }
 });
