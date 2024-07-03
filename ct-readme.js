a0aca64a5b5831d88b0e5245acb934744139e186
Brad Czerniak <ao5357@gmail.com>
2024-07-03 11:02:49 -0400

    Resolves #0 by waiting until the page has loaded.

--- ct-readme.js
+++ ct-readme.js
@@ -3,51 +3,61 @@
  * All custom overrides behaviors for the readme.
  */
 
-const link = document.createElement('link');
-link.type = 'text/css';
-link.rel = 'stylesheet';
-link.href = 'https://unpkg.com/tippy.js@6/dist/tippy.css';
-document.head.appendChild(link);
+/**
+ * Wait until all the other stuff happens to load in the dropdown.
+ */
+window.addEventListener("load", (event) => {
+  // Add in default tippy styles.
+  const link = document.createElement('link');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  link.href = 'https://unpkg.com/tippy.js@6/dist/tippy.css';
+  document.head.appendChild(link);
 
-function loadScript(src) {
-  return new Promise(resolve => {
-      const script = document.createElement("script");
-      script.setAttribute("async", "");
-      script.onload = resolve;
-      script.setAttribute("src", src);
-      document.head.appendChild(script);
-  });
-}
+  function loadScript(src) {
+    return new Promise(resolve => {
+        const script = document.createElement("script");
+        script.setAttribute("async", "");
+        script.onload = resolve;
+        script.setAttribute("src", src);
+        document.head.appendChild(script);
+    });
+  }
 
-const urls = [
-  "https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js",
-  "https://unpkg.com/tippy.js@6/dist/tippy.umd.min.js"
-];
-Promise.all(urls.map(loadScript)).then(ready);
+  // Load popper and tippy asynchronously.
+  const urls = [
+    "https://unpkg.com/@popperjs/core@2/dist/umd/popper.min.js",
+    "https://unpkg.com/tippy.js@6/dist/tippy.umd.min.js"
+  ];
+  Promise.all(urls.map(loadScript)).then(ready);
 
-function ready() {
-  var solutions = document.querySelectorAll(".rm-Header-bottom a.Button[href='/page/solutions']");
-  Array.prototype.forEach.call(solutions, (elem) => {
-    const dropdownRaw = document.createElement("div");
-    dropdownRaw.classList.add('Dropdown', 'Dropdown_closed', 'ct-solutions-dropdown');
-    dropdownRaw.innerHTML= `
-      <div class="Dropdown-toggle" aria-haspopup="dialog" aria-expanded="false">
-        <button class="rm-Header-link rm-Header-bottom-link undefined Button Button_slate_text Button_md" type="button">
-          <i class="icon-document2"></i>
-          <span>Solutions</span>
-          <i class="undefined icon-chevron-down"></i>
-        </button>
-      </div>
-    `;
-    elem.replaceWith(dropdownRaw);
-  });
+  /**
+   * Swap Solutions nav link for a dropdown button and put tippy on it.
+   */
+  function ready() {
+    var solutions = document.querySelectorAll(".rm-Header-bottom a.Button[href='/page/solutions']");
+    Array.prototype.forEach.call(solutions, (elem) => {
+      const dropdownRaw = document.createElement("div");
+      dropdownRaw.classList.add('Dropdown', 'Dropdown_closed', 'ct-solutions-dropdown');
+      dropdownRaw.innerHTML= `
+        <div class="Dropdown-toggle" aria-haspopup="dialog" aria-expanded="false">
+          <button class="rm-Header-link rm-Header-bottom-link undefined Button Button_slate_text Button_md" type="button">
+            <i class="icon-document2"></i>
+            <span>Solutions</span>
+            <i class="undefined icon-chevron-down"></i>
+          </button>
+        </div>
+      `;
+      elem.replaceWith(dropdownRaw);
+    });
 
-  tippy('.ct-solutions-dropdown', {
-    allowHTML: true,
-    arrow: false,
-    content: 'Solutions menu coming soon',
-    interactive: true,
-    placement: 'bottom',
-    trigger: 'click'
-  });
-}
+    tippy('.ct-solutions-dropdown', {
+      allowHTML: true,
+      arrow: false,
+      content: 'Solutions menu coming soon',
+      interactive: true,
+      placement: 'bottom',
+      trigger: 'click'
+    });
+  }
+});
